daeaec71d04869f584724f734031e5e2cb9882bb
Nikita Puhovskii <cheliojul@gmail.com>
2021-04-08 17:20:22 +0300

    prebuild 20%

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
-'use strict';
+// 'use strict';
+
+// let Todolist = [];
+
+// const form = document.body.getElementsById('form');
+// const button = document.body.getElementsById('Add');
+// const list = document.body.getElementsById('list');
+
+// form.addEventListener('sumbit', (submitEvent) => {
+//   submitEvent.preventDefault();
+
+//   const Todo = input.value;
+
+//   if (Todo) {
+
+//   }
+// });
+// Todolist.map(singeTodo => {
+//   const Todo = list.createElement('li');
+//   const textArea = Todo.createElement('textarea');
+// })
+
+// document.body.append(Todo);
