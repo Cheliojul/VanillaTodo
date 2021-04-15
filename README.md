95e597d2fb960d46daa0f6f3136176e74f57ab99
Nikita Puhovskii <cheliojul@gmail.com>
2021-04-15 19:49:12 +0300

    created todoList

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
+# Getting Started with Todo List
+
+Created Todo List with only Vanilla JS / SCSS
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,113 +1,12 @@
-'use strict';
-
 const { localStorage } = window;
 const list = document.querySelector('.Todo-board');
-const addButtons = document.querySelectorAll('.button');
+const addButtons = document.querySelectorAll('.plus__container, .button');
 let localTodos = JSON.parse(localStorage.getItem('todos'));
-
-const initialTodoList = [
-  {
-    id: 1,
-    date: '21 Jan 2021',
-    text: 'initialState',
-    isCompleted: false,
-  },
-];
 const scopeCount = document.querySelector('.counter__scope');
 const activeCount = document.querySelector('.counter__active');
 const completedCount = document.querySelector('.counter__completed');
 
-if (localTodos === null) {
-  localTodos = initialTodoList;
-}
-
-function renderList() {
-  if (localTodos.length < 1) {
-    const element = document.createElement('div');
-
-    element.className = `Todo-board__empty`;
-    element.innerHTML = `Create new task by clicking the button below`;
-    list.append(element);
-  }
-  document.querySelector('.Todo-board').firstChild.remove();
-
-  localTodos.map(singleTodo => {
-    const ListElement = document.createElement('li');
-
-    ListElement.class = 'Todo-board__Todo';
-
-    ListElement.innerHTML = `
-    <div key="${singleTodo.id}" class="Todo-board__todo">
-      
-      <span class="Todo-board__heading">
-        <input
-          type="checkbox"
-          id="status-${singleTodo.id}"
-          name="status"
-          cheked="${singleTodo.isCompleted}"
-          class="Todo-board__status custom-checkbox"
-        >
-        <label for="status-${singleTodo.id}"></label>
-        <span class="Todo-board__date">${singleTodo.date}</span>
-        <div class="Todo-board__edit"> </div>
-        <div class="Todo-board__delete"> </div>
-      </span>
-      
-      
-      <textarea
-        id="Text"
-        class="Todo-board__text"
-        placeholder="Please enter task description"
-        required
-        min="1"
-        max="256"
-        maxlength="256"
-      >${singleTodo.text}</textarea>
-    </div>`;
-    list.append(ListElement);
-  });
-
-  completedCount.innerHTML = localTodos
-    .reduce((acc, todo) => {
-      if (todo.isCompleted) {
-        return acc + 1;
-      }
-
-      return acc;
-    }, 0);
-  scopeCount.innerHTML = localTodos.length;
-  activeCount.innerHTML = localTodos.length - completedCount.innerHTML;
-
-  const deleteButtons = document.querySelectorAll('.Todo-board__delete');
-  const statusCheckboxes = document.querySelectorAll('.Todo-board__status');
-  const textInputs = document.querySelectorAll('.Todo-board__text');
-  // const buttonEdit = document.querySelectorAll('#Edit');
-
-  deleteButtons.forEach(button => handleDelete(button));
-  statusCheckboxes.forEach(checkbox => handleStatus(checkbox));
-  textInputs.forEach(input => handleTextInputs(input));
-
-  localStorage.setItem('todos', JSON.stringify(localTodos));
-}
-
 renderList();
-
-// buttonEdit.forEach(button => button.addEventListener('click', (e) => {
-//   let todoKey = e.target.closest('div[key]').getAttribute('key');
-//   let targetInput = localTodos.find(todo => todo.id === +todoKey);
-//   targetInput.text = e.target.value
-// }));
-
-function getTodayDate() {
-  const monthList = ['Jan', 'Feb', 'Mar', 'Apr',
-    'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  const today = new Date();
-  const dd = String(today.getDate()).padStart(2, '0');
-  const mm = String(today.getMonth());
-  const yyyy = today.getFullYear();
-
-  return dd + ' ' + monthList[mm] + ' ' + yyyy;
-}
 
 addButtons.forEach(button => {
   button.addEventListener('click', () => {
@@ -131,6 +30,29 @@ addButtons.forEach(button => {
   });
 });
 
+function handleEdit(button) {
+  button.addEventListener('click', (e) => {
+    const currentTextInput = e.target.offsetParent.nextElementSibling;
+
+    if (currentTextInput.hasAttribute(`disabled`)) {
+      currentTextInput.removeAttribute(`disabled`);
+    } else {
+      currentTextInput.setAttribute(`disabled`, `disabled`);
+    }
+  });
+}
+
+function getTodayDate() {
+  const monthList = ['Jan', 'Feb', 'Mar', 'Apr',
+    'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth());
+  const yyyy = today.getFullYear();
+
+  return dd + ' ' + monthList[mm] + ' ' + yyyy;
+}
+
 function handleDelete(button) {
   button.addEventListener('click', (e) => {
     const currentTodoKey = e.target.closest('div[key]').getAttribute('key');
@@ -150,15 +72,8 @@ function handleStatus(box) {
     const currentTodoKey = e.target.closest('div[key]').getAttribute('key');
     const currentTodo = localTodos.find(todo => todo.id === +currentTodoKey);
     const elements = document.querySelectorAll('.Todo-board__todo');
-    const currentTextArea = e.target.offsetParent.nextElementSibling;
 
     currentTodo.isCompleted = !currentTodo.isCompleted;
-
-    if (currentTodo.isCompleted) {
-      currentTextArea.setAttribute('readonly', 'readonly');
-    } else {
-      currentTextArea.removeAttribute('readonly');
-    }
     elements.forEach(element => element.remove());
     renderList();
   });
@@ -174,4 +89,80 @@ function handleTextInputs(inputElement) {
     elements.forEach(element => element.remove());
     renderList();
   });
+}
+
+function createTodo(todo) {
+  const ListElement = document.createElement('li');
+
+  ListElement.class = 'Todo-board__Todo';
+
+  ListElement.innerHTML = `
+  <div key="${todo.id}" class="Todo-board__todo">
+    
+    <span class="Todo-board__heading">
+      <input
+        type="checkbox"
+        id="status-${todo.id}"
+        name="status"
+        ${todo.isCompleted ? `checked="${todo.isCompleted}"` : ``}
+        class="Todo-board__status custom-checkbox"
+      >
+      <label for="status-${todo.id}"></label>
+      <span class="Todo-board__date">${todo.date}</span>
+      <div class="Todo-board__edit"> </div>
+      <div class="Todo-board__delete"> </div>
+    </span>
+    
+    
+    <textarea
+      id="Text"
+      ${todo.isCompleted ? `style="text-decoration:line-through;"` : ``}
+      class="Todo-board__text"
+      placeholder="Please enter task description"
+      required
+      min="1"
+      max="256"
+      disabled
+    >${todo.text}</textarea>
+  </div>`;
+  list.append(ListElement);
+};
+
+function renderList() {
+  if (localTodos.length < 1) {
+    const element = document.createElement('div');
+
+    element.className = `Todo-board__empty`;
+    element.innerHTML = `Create new task by clicking the button below`;
+    list.append(element);
+  } else {
+    if (document.querySelector('.Todo-board__empty')) {
+      document.querySelector('.Todo-board__empty').remove();
+    }
+  }
+
+  localTodos.map(singleTodo => createTodo(singleTodo));
+
+  completedCount.innerHTML = localTodos
+    .reduce((acc, todo) => {
+      if (todo.isCompleted) {
+        return acc + 1;
+      }
+
+      return acc;
+    }, 0);
+  scopeCount.innerHTML = localTodos.length;
+  activeCount.innerHTML = localTodos.length - completedCount.innerHTML;
+
+  const deleteButtons = document.querySelectorAll('.Todo-board__delete');
+  const statusCheckboxes = document.querySelectorAll('.Todo-board__status');
+  const textInputs = document.querySelectorAll('.Todo-board__text');
+  const editButtons = document.querySelectorAll('.Todo-board__edit');
+
+  deleteButtons.forEach(button => handleDelete(button));
+  statusCheckboxes.forEach(checkbox => handleStatus(checkbox));
+  textInputs.forEach(input => handleTextInputs(input));
+  editButtons.forEach(button => handleEdit(button));
+
+  localStorage.setItem('todos', JSON.stringify(localTodos));
 }
